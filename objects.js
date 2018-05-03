45a890f0020d14cd7d98f7b6673340516773836c
Sandy Edwards <peterjamesedwards@gmail.com>
2018-05-03 15:12:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,6 @@ function updatePlaylist (playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  var playlist = { Kanye: 'Gold Digger' };
 delete playlist.artistName;
 return playlist;
   
